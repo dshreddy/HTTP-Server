5f9ad08ab980495fa5c33a55c239d5ca13cb1d36
dshreddy <dshreddy03@gmail.com>
2025-07-07 13:20:46 +0530

    init commit

--- app/main.py
+++ app/main.py
@@ -2,13 +2,11 @@ import socket  # noqa: F401
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
+    # Create a server socket on bind it to port 4221
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
 
-    # Uncomment this to pass the first stage
-    #
-    # server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    # server_socket.accept() # wait for client
+    # Wait for client connections
+    server_socket.accept() 
 
 
 if __name__ == "__main__":
